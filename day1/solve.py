3c8c17b8f752382c9833153a40f35997e347e93c
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2019-11-30 23:13:28 -0800

    day1

--- day1/solve.py
+++ day1/solve.py
@@ -1,9 +1,21 @@
 # DAY 1 - PROBLEM 1
-file1 = open("day1/input1.txt")
-for line in file1:
-    print(line.strip())
+file = open("day1/input.txt")
+data = [int(s.strip()) for s in file.readlines()]
+ans = sum([i//3-2 for i in data])
+
+print(ans)
+
 
 # DAY 1 - PROBLEM 2
-file2 = open("day1/input2.txt")
-for line in file2:
-    print(line.strip())
+file = open("day1/input.txt")
+data = [int(s.strip()) for s in file.readlines()]
+total = []
+for i in data:
+    cost = i//3-2
+    next = cost//3-2
+    while next > 0:
+        cost += next
+        next = next//3-2
+    total.append(cost)
+
+print(sum(total))
