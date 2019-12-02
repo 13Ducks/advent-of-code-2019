d30cf1201cda74405cce1aa6214bc4dc1800d7ff
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2019-12-01 21:39:51 -0800

    clean

--- day2/solve.py
+++ day2/solve.py
@@ -1,4 +1,5 @@
 # DAY 2
+
 file = open("day2/input.txt")
 input = list(map(int, file.readline().split(",")))
 
@@ -6,19 +7,14 @@ def solve(noun,verb):
     data = input.copy()
     data[1] = noun
     data[2] = verb
-    i = 0
-    while i < len(data):
+
+    for i in range(0, len(data), 4):
         if data[i] == 1:
-            print
             data[data[i+3]] = data[data[i+1]] + data[data[i+2]]
-            i+=4
         elif data[i] == 2:
             data[data[i+3]] = data[data[i+1]] * data[data[i+2]]
-            i+=4
         elif data[i] == 99:
             break
-        else:
-            i+=1
     return data[0]
 
 print(solve(12,2))
