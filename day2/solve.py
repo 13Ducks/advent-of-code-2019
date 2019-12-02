fea8af612c54c67937af8839b9dff1de1f17f3f8
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2019-12-01 21:32:01 -0800

    day2 done

--- day2/solve.py
+++ day2/solve.py
@@ -1,6 +1,32 @@
 # DAY 2
-
 file = open("day2/input.txt")
-data = map(lambda x: x.strip(), file.readlines())
-for i in data:
-    print(i)
+input = list(map(int, file.readline().split(",")))
+
+def solve(noun,verb):
+    data = input.copy()
+    data[1] = noun
+    data[2] = verb
+    i = 0
+    while i < len(data):
+        if data[i] == 1:
+            print
+            data[data[i+3]] = data[data[i+1]] + data[data[i+2]]
+            i+=4
+        elif data[i] == 2:
+            data[data[i+3]] = data[data[i+1]] * data[data[i+2]]
+            i+=4
+        elif data[i] == 99:
+            break
+        else:
+            i+=1
+    return data[0]
+
+print(solve(12,2))
+
+for n in range(100):
+    for v in range(100):
+        ans = solve(n,v)
+        if ans == 19690720:
+            print(n,v)
+            print(100*n+v)
+            break
