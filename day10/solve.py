f1d3277aeb012741768038fee7023d889048288d
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2019-12-12 21:24:44 -0800

    faster solve

--- day10/solve.py
+++ day10/solve.py
@@ -3,64 +3,47 @@ import math
 
 file = open("day10/input.txt")
 input = list(map(lambda x: x.strip(), file.readlines()))
-astroids = set()
+asteroids = set()
 sign = lambda a: (a>0) - (a<0)
 
 for i in range(len(input)):
     for j in range(len(input[0])):
-        if input[i][j] == '#': astroids.add((j,i))
+        if input[i][j] == '#': asteroids.add((j,i))
 
-def inter(orig, target, between):
-    match_sign = (sign(target[1]-orig[1]) == sign(between[1]-orig[1])) and (sign(target[0]-orig[0]) == sign(between[0]-orig[0]))
-    try:
-        s1 = (target[1]-orig[1])/(target[0]-orig[0])
-        s2 = (between[1]-orig[1])/(between[0]-orig[0])
-        if s1 == s2 and match_sign:
-            return True
-        return False
-    except ZeroDivisionError:
-        if target[0]-orig[0] == between[0]-orig[0] and match_sign:
-            return True
-        return False
-
-def dist(orig, target, between):
-    return (abs(orig[0]-target[0])+abs(orig[1]-target[1])) > (abs(orig[0]-between[0])+abs(orig[1]-between[1]))
-
-see = {}
-intersect = {}
-max_val = (11,13)
-for a_orig in astroids: 
-    if a_orig == (max_val):
-        c = 0
-        for a_target in astroids:
-            in_sight = not a_orig == a_target
-            all_between = set()                
-            for a_between in astroids:
-                if a_target != a_orig and a_target != a_between and inter(a_orig, a_target, a_between) and dist(a_orig, a_target, a_between):
-                    in_sight = False
-                    all_between.add(a_between)
-            if in_sight: c+=1
-            intersect[a_target] = all_between
-        see[a_orig] = c
-
-ans = max(see, key=lambda key: see[key])
-print(ans, see[ans])
+inter_count = set()
+for a_orig in asteroids:
+    all_angles = set()
+    for a_other in asteroids:
+        if a_orig != a_other:
+            all_angles.add((math.degrees(math.atan2(a_other[1]-a_orig[1],a_other[0]-a_orig[0]))))
+    inter_count.add((a_orig, len(all_angles)))
+ans = max(inter_count, key=lambda x: x[1])
+print(ans)
 
 angles = {}
-for a in astroids:
-    if a != max_val:
-        angles[a] = (450 - (-math.degrees(math.atan2(a[1]-max_val[1],a[0]-max_val[0])))) % 360
+for a in asteroids:
+    if a != ans[1]:
+        angles[a] = (450 - (-math.degrees(math.atan2(a[1]-ans[0][1],a[0]-ans[0][0])))) % 360
 
 angles = {k: v for k, v in sorted(angles.items(), key=lambda item: item[1])}
 
+def dist(a1, a2): return abs(a2[0]-a1[0]) + abs(a2[1]-a1[1])
+
+inter = {}
+for i in asteroids:
+    curr_inter = set()
+    for j in asteroids:
+        if i != ans[0] and j != ans[0] and i != j and angles[i] == angles[j] and dist(ans[0], i) > dist(ans[0], j):
+            curr_inter.add(j)
+    inter[i] = curr_inter
+
+# won't actually destroy every asteroid
 destroyed = []
-last_angle = 1000
-# DOES NOT WORK FOR EXAMPLE BOTH PARTS PLS FIX
+last_angle = -1
 for x in range(100):
     for i in angles.items():
         if i[0] not in destroyed:
-            #print(i)
-            i_inter = intersect[i[0]]
+            i_inter = inter[i[0]]
             next_destroy = True
             for j in i_inter:
                 if j not in destroyed:
@@ -69,4 +52,5 @@ for x in range(100):
                 destroyed.append(i[0]) 
                 last_angle = i[1]
 
-print(destroyed[199])
+sol = destroyed[199]
+print(sol[0]*100+sol[1])
