9fc5eaff43b8371ac83284101be32f5a4d655664
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2019-12-16 17:38:17 -0800

    finally did day 7

--- day7/solve.py
+++ day7/solve.py
@@ -1,87 +1,88 @@
-# DAY 5
+# DAY 7
 
 import itertools
 
 file = open("day7/input.txt")
-input_data = list(map(int, file.readline().split(",")))
+input = list(map(int, file.readline().split(",")))
 
-def solve(phase, val, data, first_input, i):
+def intcode(code):
+    i = 0
+    num_args = {'01':3,'02':3,'03':1,'04':1,'05':2,'06':2,'07':3,'08':3,'99':0}
+
     def index(param, off):
         if param == '0':
-            return data[i+off]
+            return code[i+off]
         else:
             return i+off
 
     while True:
-        op = str(data[i])[-2:].zfill(2)
-        param = str(data[i])[:-2].zfill(3)
+        op = str(code[i])[-2:].zfill(2)
+        param = str(code[i])[:-2].zfill(3)
+        
+        if num_args[op] >= 1: arg1 = int(index(param[2], 1))
+        if num_args[op] >= 2: arg2 = int(index(param[1], 2))
+        if num_args[op] >= 3: arg3 = int(index(param[0], 3))
+        jump = False
+
         if op == '01':
-            data[int(index(param[0], 3))] = data[int(index(param[2], 1))] + data[int(index(param[1], 2))]
-            i+=4
+            code[arg3] = code[arg1] + code[arg2]
         elif op == '02':
-            data[int(index(param[0], 3))] = data[int(index(param[2], 1))] * data[int(index(param[1], 2))]
-            i+=4
+            code[arg3] = code[arg1] * code[arg2]
         elif op == '03':
-            if not first_input:
-                data[int(index(param[2], 1))] = phase
-                first_input = True
-            else:
-                data[int(index(param[2], 1))] = val
-            i+=2
+            code[arg1] = yield 'needinput'
         elif op == '04':
-            return [data[int(index(param[2], 1))], data, i]
+            yield code[arg1]
         elif op == '05':
-            if data[int(index(param[2], 1))] != 0:
-                i = data[int(index(param[1], 2))]
-            else:
-                i+=3
+            if code[arg1] != 0:
+                i = code[arg2]
+                jump = True
         elif op == '06':
-            if data[int(index(param[2], 1))] == 0:
-                i = data[int(index(param[1], 2))]
-            else:
-                i+=3
+            if code[arg1] == 0:
+                i = code[arg2]
+                jump = True
         elif op == '07':
-            data[int(index(param[0], 3))] = int(data[int(index(param[2], 1))] < data[int(index(param[1], 2))])
-            i+=4
+            code[arg3] = int(code[arg1] < code[arg2])
         elif op == '08':
-            data[int(index(param[0], 3))] = int(data[int(index(param[2], 1))] == data[int(index(param[1], 2))])
-            i+=4
+            code[arg3] = int(code[arg1] == code[arg2])
         elif op == '99':
-            return False
+            yield 'break'
+            break
         else:
             print(op)
+        
+        if not jump: i+=num_args[op]+1
 
 all = list(itertools.permutations([0,1,2,3,4]))
 out = []
 for a in all:
     o = 0
     for l in a:
-        o = solve(l,o,input_data.copy(),False,0)[0]
+        comp = intcode(input.copy())
+        next(comp)
+        comp.send(l)
+        o = comp.send(o)
     out.append(o)
 print(max(out))
 
-all = [[9,8,7,6,5]] #should produce 139629729
+all = list(itertools.permutations([5,6,7,8,9]))
 out = []
-for a in all:
-    curr_data = input_data.copy()
-    o = 0
-    amps = []
-    for l in a:
-        res = solve(l,o,curr_data,False,0)
-        print(res)
-        amps.append(res.copy())
-        o = res[0]
-    for b in amps:
-        print(b)
-    # for i in range(1):
-    #     for i in range(5):
-    #         res = solve("",o,amps[i][0],True,amps[i][1]+2)
-    #         if not res:
-    #             print(i,"DONE")
-    #             break
-    #         o = res[0]
-    #         amps[0] = (res[1],res[2])
-    #         print(o)
+for p in all:
+    amps = [intcode(input.copy()) for _ in range(5)]
+    o=0
+    for i in range(len(amps)):
+        a = amps[i]
+        next(a)
+        a.send(p[i])
+        o = a.send(o)
 
-    # for a in amps:
-    #     print(a)
+    done = False
+    while not done:
+        for i in range(len(amps)):
+            a = amps[i]
+            try:
+                next(a)
+                o = a.send(o)
+            except StopIteration:
+                done = True
+    out.append(o)
+print(max(out))
