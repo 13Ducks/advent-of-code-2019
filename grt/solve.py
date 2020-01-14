819895709a3a3adb7745aa101f299673fcbaf0bd
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2020-01-13 19:15:55 -0800

    more vision

--- grt/solve.py
+++ grt/solve.py
@@ -1,117 +1,121 @@
 import cv2
 import numpy as np
 import time
+import socket
 
-vid = cv2.VideoCapture("grt/bigtest.mov")
+HOST = ''  # Standard loopback interface address (localhost)
+PORT = 30000        # Port to listen on (non-privileged ports are > 1023)
 
 vision_tape_area = 110.85
 bounding_box = (40, 17)
 bounding_box_area = bounding_box[0] * bounding_box[1]
 coverage_area = vision_tape_area / bounding_box_area
-coverage_weight = 100
 bounding_aspect = bounding_box[0] / bounding_box[1]
-aspect_weight = 50
 
-max_diff_allow = 80
+max_diff_allow = 85
 
-kHorizontalFOVDeg = 61
+kHorizontalFOVDeg = 62.8
+kVerticalFOVDeg = 37.9
+
 kTargetHeightIn = 8*12 + 2.25 #middle of hex height
-kCameraHeightIn = 15
-kCameraPitchDeg = 50
-kVerticalFOVDeg = 0
+kCameraHeightIn = 6
+kCameraPitchDeg = 40
 
-while(True):
-    begin = time.time()
-    ret, frame = vid.read()
-    #frame = cv2.flip(frame, 0)
-    kVerticalFOVDeg = frame.shape[0]/frame.shape[1]*kHorizontalFOVDeg
-    #print(frame.shape)
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    # lower_green = np.array([0,0,240])
-    # upper_green = np.array([20,20,255])
-    lower_green = np.array([30,0,250])
-    upper_green = np.array([100,10,255])
-    mask = cv2.inRange(hsv, lower_green, upper_green)
-    res = cv2.bitwise_and(frame, frame, mask=mask)
-    
-    def onmouse(k, x, y, s, p):
-        global hsv
-        if k == 1:
-            print(hsv[y, x])
+def nothing(x): pass
 
-    cv2.namedWindow("hsv", cv2.WINDOW_NORMAL)
-    cv2.setMouseCallback("hsv", onmouse)
-    cv2.imshow("hsv", hsv)
+cv2.namedWindow("Output", cv2.WINDOW_NORMAL)
+cv2.createTrackbar('H_min','Output',0,255,nothing)
+cv2.createTrackbar('H_max','Output',200,255,nothing)
+cv2.createTrackbar('S_min','Output',0,255,nothing)
+cv2.createTrackbar('S_max','Output',10,255,nothing)
+cv2.createTrackbar('V_min','Output',250,255,nothing)
+cv2.createTrackbar('V_max','Output',255,255,nothing)
+cv2.createTrackbar('Angle','Output',kCameraPitchDeg,50,nothing)
 
-    cv2.namedWindow("Output", cv2.WINDOW_NORMAL)
-    cv2.setMouseCallback("Output",onmouse);
+cap = cv2.VideoCapture("grt/bigtest.mov")
 
-    imgray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(imgray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(
-        thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+if not cap.isOpened(): 
+    print("Error opening stream")
 
-    max_contour = (0, 0, 0, 0)
 
-    for cnt in contours:
-        # area_cnt = cv2.contourArea(cnt)
-        # if max_contour[3] < area_cnt:
-        #     rect = cv2.minAreaRect(cnt)
-        #     box = cv2.boxPoints(rect)
-        #     box = np.int0(box)
-        #     area_box = cv2.contourArea(box)
-        #     max_contour = ([box], rect, area_box, area_cnt)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        range = 0
+        azimuth = 0
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            if not ret: continue
 
-        box = cv2.boundingRect(cnt)
-        area = box[2]*box[3]
-        if area > 10000:
-            #print(area)
-            diff_coverage = 100 - coverage_weight*abs(cv2.contourArea(cnt)/area - coverage_area)
-            diff_aspect = 100 - aspect_weight*abs(box[2]/box[3] - bounding_aspect)
-            diff_sum = (diff_aspect + diff_coverage)/2
-            if diff_sum > 80:
-                #print(diff_coverage, diff_aspect)
-                max_contour = (box, area, cv2.contourArea(cnt), cnt)
+            hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+            def onmouse(k, x, y, s, p):
+                if k == 1:
+                    print(hsv[y, x])
+
+            cv2.setMouseCallback("Output",onmouse);
+
+            lower_green = np.array([cv2.getTrackbarPos('H_min','Output'),cv2.getTrackbarPos('S_min','Output'),cv2.getTrackbarPos('V_min','Output')])
+            upper_green = np.array([cv2.getTrackbarPos('H_max','Output'),cv2.getTrackbarPos('S_max','Output'),cv2.getTrackbarPos('V_max','Output')])
+            mask = cv2.inRange(hsv, lower_green, upper_green)
+            res = cv2.bitwise_and(frame, frame, mask=mask)
+
+            imgray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
+            blur = cv2.blur(imgray, (5, 5))
+            ret, thresh = cv2.threshold(blur, 127, 255, 0)
+            if not ret: continue
+
+            contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            best_contour = (0, 0, 0, 0)
+
+            for cnt in contours:
+                rect = cv2.minAreaRect(cnt)
+                box = np.int0(cv2.boxPoints(rect))
+                area = cv2.contourArea(box)
+                
+                if area > 8000:
+                    diff_coverage = 100 - 100*abs(cv2.contourArea(cnt)/area - coverage_area)
+                    width = max(rect[1])
+                    height = min(rect[1])
+                    diff_aspect = 100 - 100*abs(width/height - bounding_aspect)
+                    diff_avg = (diff_aspect + diff_coverage)/2
+                    
+                    if diff_avg > max_diff_allow:
+                        best_contour = ([box], rect, area)
             
-    if max_contour[0] != 0:        
-        x,y,w,h = max_contour[0]
+            if best_contour[0] != 0: 
+                center, dim, angle = best_contour[1]
+                
+                if dim[0] < dim[1]:
+                    dim = [dim[1], dim[0]]
+                    angle += 90
 
-        diff_coverage = 100 - coverage_weight*abs(max_contour[2]/max_contour[1] - coverage_area)
-        diff_aspect = 100 - aspect_weight*abs(w/h - bounding_aspect)
-        diff_sum = (diff_aspect + diff_coverage)/2
-        #print(diff_sum)
+                x = int(center[0] + 0.5 * dim[1] * np.sin(np.radians(angle)))
+                y = int(center[1] - 0.5 * dim[1] * np.cos(np.radians(angle)))
+                
+                x_scale = 2 * (x / frame.shape[1]) - 1
+                y_scale = -(2 * (y / frame.shape[0]) - 1);
+                
+                cv2.drawContours(res, best_contour[0], 0, (0, 0, 255), 2)
+                cv2.drawMarker(res, tuple(map(int,(x,y))), (0,255,0), thickness=2)
+                cv2.putText(res,"x: " + str(round(x_scale,4)), (int(x),int(y)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255))
+                cv2.putText(res,"y: " + str(round(y_scale,4)), (int(x),int(y)+25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0,0,255))
+            
+                azimuth = (x_scale * kHorizontalFOVDeg / 2.0) % 360
+                kCameraPitchDeg = cv2.getTrackbarPos('Angle','Output')
+                range = (kTargetHeightIn - kCameraHeightIn) / np.tan(np.radians(y_scale * (kVerticalFOVDeg / 2.0) + kCameraPitchDeg))
+                print(range, azimuth, range*np.cos(np.radians(kCameraPitchDeg)), range*np.sin(np.radians(kCameraPitchDeg)))
 
-        frame_reso = (frame.shape[1]/2, frame.shape[0]/2)
-        # print(x,(x-frame_reso[0])/frame_reso[0])
-        # print(y,-(y-frame_reso[1])/frame_reso[1])
-        # print(x+w,(x+w-frame_reso[0])/frame_reso[0])
-        # print(y+h,-(y+h-frame_reso[1])/frame_reso[1])
+            cv2.imshow('Output', res)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
-        #if diff_sum > max_diff_allow:
-            # cv2.drawContours(res, max_contour[0], 0, (0, 0, 255), 2)
-            # cv2.circle(res, (int(max_contour[1][0][0] + 0.5 * max_contour[1][1][1] * np.sin(np.radians(max_contour[1][2]))), int(
-            #     max_contour[1][0][1] - 0.5 * max_contour[1][1][1]  * np.cos(np.radians(max_contour[1][2])))), 20, (254, 0, 0), 20)
-        cv2.rectangle(res,(x,y),(x+w,y+h),(0,255,0),2)
-        # print((bounding_box[0]/12)*frame.shape[1]/(2*w*np.tan(np.radians(40))))
-
-        x_scaled = x+w/2
-        x_scaled = 2 * (x_scaled / frame.shape[1]) - 1;
-        y_scaled = -(2 * (y / frame.shape[0]) - 1);
-        cv2.putText(res,"x: " + str(round(x_scaled,4)), (int(x+w/2),y), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255))
-        cv2.putText(res,"y: " + str(round(y_scaled,4)), (int(x+w/2),int(y-50)), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255))
-        # cv2.putText(res,"x: " + str(round((x+w-frame_reso[0])/frame_reso[0],4)), (x+w,y+h), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255))
-        # cv2.putText(res,"y: " + str(-round((y+h-frame_reso[1])/frame_reso[1],4)), (x+w,y+h-50), cv2.FONT_HERSHEY_SIMPLEX, 2, (0,0,255))
-
-        cv2.imshow('Output', res)
-        
-        # #print(time.time() - begin)
-        kVerticalFOVDeg = frame.shape[0]/frame.shape[1]*kHorizontalFOVDeg
-        azimuth = (x_scaled * kHorizontalFOVDeg / 2.0) % 360
-        range = (kTargetHeightIn - kCameraHeightIn) / np.tan(np.radians(y_scaled * kVerticalFOVDeg / 2.0 + kCameraPitchDeg))
-        print(range, azimuth)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            conn.send(bytes(str((range,azimuth))+"\n","UTF-8"))
+            time.sleep(0.01)
+    
 vid.release()
 cv2.destroyAllWindows()
