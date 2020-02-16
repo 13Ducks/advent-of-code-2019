bf8fba71fd37e8ea3689e4151ffad860c6dd1871
Aaryan Agrawal <aaryanagrawal03@gmail.com>
2020-02-15 21:12:40 -0800

    updated solve.py with new code (azimuth + distance only)

--- grt/solve.py
+++ grt/solve.py
@@ -9,22 +9,24 @@ vision_tape_area = 110.85
 bounding_box = (40, 17)
 bounding_box_area = bounding_box[0] * bounding_box[1]
 coverage_area = vision_tape_area / bounding_box_area
-# bounding_aspect = bounding_box[0] / bounding_box[1]
 
-hex_ratio = 39.261 / 19.360
+hex_dim = (39.261, 19.360)
+hex_ratio = hex_dim[0] / hex_dim[1]
+hex_area = 3/2 * np.sqrt(3) * (hex_dim[0]/2)**2 / 2
+solidity_expect = vision_tape_area / hex_area
 
 max_diff_allow = 80
 min_area = 100
 min_coverage = 90
-# min_aspect = 65
-min_hex_ratio = 90
+min_solidity = 90
+min_hex_ratio = 70
 
 kHorizontalFOVDeg = 62.8
 kVerticalFOVDeg = 37.9
 
 kTargetHeightIn = 8*12 + 2.25  # middle of hex height
-kCameraHeightIn = 15
-kCameraPitchDeg = 30
+kCameraHeightIn = 24
+kCameraPitchDeg = 25
 
 lower_green = np.array([0,0,250])
 upper_green = np.array([200,10,255])
@@ -45,7 +47,6 @@ while True:
                 while(cap.isOpened()):
                     distance = 0
                     azimuth = 0
-                    pitch = 0
 
                     _, frame = cap.read()
 
@@ -55,9 +56,10 @@ while True:
                     res = cv2.bitwise_and(frame, frame, mask=mask)
 
                     imgray = cv2.cvtColor(res, cv2.COLOR_BGR2GRAY)
-                    # NOTE: median blur
-                    blur = cv2.medianBlur(imgray,7)
-                    ret, thresh = cv2.threshold(blur, 64, 255, 0)
+                    
+                    structure_element = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
+                    morphed = cv2.dilate(cv2.erode(imgray, structure_element), structure_element)
+                    ret, thresh = cv2.threshold(morphed, 64, 255, 0)
 
                     contours, hierarchy = cv2.findContours(
                         thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[-2:]
@@ -76,15 +78,16 @@ while True:
                         rect_area = cv2.contourArea(box)
 
                         diff_coverage = 100 - 100 * abs(area/rect_area - coverage_area)
-
-                        # width = max(rect[1])
-                        # height = min(rect[1])
-                        # diff_aspect = 100 - 100*abs(width/height - bounding_aspect)
                         
                         if diff_coverage < min_coverage:
                             continue
 
-                        hull = list(map(lambda x: x[0], cv2.convexHull(cnt).tolist()))
+                        hull = cv2.convexHull(cnt)
+                        hull_area = cv2.contourArea(hull)
+                        solidity = float(area)/hull_area
+                        diff_solidity = 100 - 100 * abs(solidity - solidity_expect)
+
+                        hull = list(map(lambda x: x[0], hull.tolist()))
 
                         while len(hull) > 4:
                             min_dist = [10000,0]
@@ -104,22 +107,18 @@ while True:
                         dist_bot = np.hypot(bot[0][0]-bot[1][0], bot[0][1]-bot[1][1])
                         diff_hex_ratio = 100 - 100*(hex_ratio - dist_top/dist_bot)
 
-                        if diff_hex_ratio < min_hex_ratio:
+                        if diff_hex_ratio < min_hex_ratio or diff_solidity < min_solidity:
                             continue
                         
-                        total_diff = (diff_hex_ratio + diff_coverage) / 2
-                        if total_diff > best_diff:
+                        avg_diff = (diff_hex_ratio + diff_coverage + diff_solidity) / 3
+                        if avg_diff > best_diff:
                             best_hull = hull
-                            best_diff = total_diff
+                            best_diff = avg_diff
 
                     if best_hull != 0:        
                         
                         pts1 = np.array(best_hull, np.float32)
                         x,y = [(pts1[0][0] + pts1[3][0])/2, (pts1[0][1] + pts1[3][1])/2]
-                        pts2 = np.array([[0, 0], [100, 170], [300, 170], [400, 0]], np.float32)
-
-                        matrix = cv2.getPerspectiveTransform(pts1, pts2)
-                        pitch = np.arcsin(matrix[1][0])
                     
                         x_scale = 2 * (x / frame.shape[1]) - 1
                         y_scale = -(2 * (y / frame.shape[0]) - 1)
@@ -131,7 +130,7 @@ while True:
                     if cv2.waitKey(1) & 0xFF == ord('q'):
                         break
 
-                    conn.send(bytes(str((distance,azimuth,pitch))+"\n","UTF-8"))
+                    conn.send(bytes(str((distance,azimuth))+"\n","UTF-8"))
     except (BrokenPipeError, ConnectionResetError, ConnectionRefusedError) as e:
         print("connection lost... retrying")
     
